6bb6b7f3cce24307c74997e4d01e3f7bc4e5204f
rabobahago <rabobahago@gmail.com>
2021-03-29 21:04:21 +0100

    home page completed

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,22 @@
 import './Product.css'
-const Product = () => {
+const Product = ({ id, title, image, price, rating }) => {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
-        <p className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
           <small>$</small>
-          <strong>12.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-        alt=""
-      />
+      <img src={image} alt="" />
       <button>Add to Basket</button>
     </div>
   )
