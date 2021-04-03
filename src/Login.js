07b9f4ea5587411c639da2642fa4313e16061dba
rabobahago <rabobahago@gmail.com>
2021-04-03 22:28:34 +0100

    login styled added

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const signIn = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className="login">
       <Link to="/">
@@ -11,6 +16,39 @@ const Login = () => {
           alt=""
         />
       </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
     </div>
   )
 }
